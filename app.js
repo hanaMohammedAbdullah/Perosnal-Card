2c86c665c610106630bb0ba86a50af689bb5caa1
hanohirani <hanohirani@gmail.com>
2021-12-17 00:45:24 +0300

    changing file

--- app.js
+++ app.js
@@ -6,27 +6,18 @@ const app = () => {
                 name: "salwa fkri",
                 study: "medical microbiology ",
                 hobby: "reading books, watching movie  "
-
-
-
             },
             {
                 image: "https://www.mcall.com/resizer/CUnRY8Bg6L1Wqv_Tx25GakRXb8Q=/800x1422/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/HFG4UYDXMRCRFFDW3ORRNE43JI.jpg",
                 name: "mohammed salam",
                 study: "Computer Engineering",
                 hobby: "watching movie , coding"
-
-
-
             },
             {
                 image: "https://www.mcall.com/resizer/CUnRY8Bg6L1Wqv_Tx25GakRXb8Q=/800x1422/top/arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/HFG4UYDXMRCRFFDW3ORRNE43JI.jpg",
                 name: "amanj azad",
                 study: "IT",
                 hobby: "coding , hicking"
-
-
-
             },
             {
                 image: "https://ath2.unileverservices.com/wp-content/uploads/sites/4/2020/02/IG-annvmariv-1024x1016.jpg",
